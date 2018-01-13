a6f234db9eb4118302bc424ca07f4c49da4fa3f3
Jason Green <jason@green.io>
2018-01-12 22:08:27 -0500

    finished documenting

--- papyri.py
+++ papyri.py
@@ -183,10 +183,12 @@ for datFile in playerDatFiles:
                         color = colors
                     else:
                         color = ["black"]
-                        
+
+                    # prepare a POI for writing
                     POI = (uuid, title, x,z, linkFormat.format(viewerx,viewerz), desc, num, color)
 
                     logging.info(POI)
+                    
                     # if there's tags, append to the tag dict, otherwise, add to dict as untagged
                     if tags:
                         
@@ -194,63 +196,87 @@ for datFile in playerDatFiles:
                             taggedPois[tag].append(POI)
                     else:
                         taggedPois["untagged"].append(POI)
-                        #poisFile.write("|![]({})|[{}, {}]({})|{}|{}|\n".format(head, x,z, linkFormat.format(viewerx,viewerz), title, desc))
+                    
 
 logging.info("Created %s tags", len(taggedPois))
 
+# create the output folders if they don't exsist
 if not os.path.exists(papyriOutputPath):
     logging.info("Creating output folders")
     os.makedirs(papyriOutputPath)
     os.makedirs(os.path.join(papyriOutputPath, "map"))
 
+    # copy the web template files to the output folders
     logging.info("Copying template web files")
     shutil.copy(os.path.join("template", "index.html"), papyriOutputPath)
     shutil.copy(os.path.join("template", "map", "index.html"), os.path.join(papyriOutputPath, "map"))
     shutil.copy(os.path.join("template", "map", "script.js"), os.path.join(papyriOutputPath, "map"))
     shutil.copy(os.path.join("template", "map", "style.css"), os.path.join(papyriOutputPath, "map"))
     
-logging.info("Writing index.md containg POI")    
+logging.info("Writing index.md containg POI")
+
+# write the index.md file containing all the POI
 with open(os.path.join(papyriOutputPath, "index.md"), "w", encoding="utf-8") as poisFile:
+
+    # write the header
     poisFile.write(fileHeader)
+
+    # iterate over each tag
     for tag in sorted(taggedPois):
+        #write the header for the tag
         poisFile.write("## {}".format(tag))
         poisFile.write(tableHeader)
+
+        # iterate over all the POI in the tag
         for poi in taggedPois[tag]:
             poisFile.write(poiFormat.format(*poi))
 
         
 
-
+# path to maps
 mapsInputGlob = os.path.join(mcdata, "data", "map*.dat")
 
+# dimesnion value to anme dict
 dimDict = {-1: "nether", 0: "overworld", 1: "end"}
 
+# get all the maps
 mapFiles = glob.glob(mapsInputGlob)
-logging.info("Found %s maps", len(mapFiles))
 
+# sort the maps by modified time
 mapFiles.sort(key=os.path.getmtime)
 
-background = {d: PIL.Image.new('RGBA', (canvasSize, canvasSize), (0, 0, 0, 255)) for d in dimDict}
-bg = {d: pyvips.Image.black(canvasSize, canvasSize) for d in dimDict}
+logging.info("Found %s maps", len(mapFiles))
 
+# create the output map images, one per dimension
+background = {d: PIL.Image.new('RGBA', (canvasSize, canvasSize), (0, 0, 0, 255)) for d in dimDict}
+#bg = {d: pyvips.Image.black(canvasSize, canvasSize) for d in dimDict}
+
+# create a list for the map objects
 mapFileObjs = []
 
 logging.info("Parsing map .dat files")
+
+# get all the map objects
 for mapFile in mapFiles:
     mapFileObjs.append(minecraftmap.Map(mapFile,eco=False))
 
+# sort them by zoom level
 mapFileObjs.sort(key=lambda m: m.zoomlevel, reverse=True)
 
-
+# iterate over all the map objects
 for m in mapFileObjs:
     dimension = m.dimension
     zoom = str(m.zoomlevel)
     x = int((m.centerxz[0] * scaleFactor ) + canvasSize / 2 - 128 * 2 ** m.zoomlevel / 2 * scaleFactor)
     z = int((m.centerxz[1] * scaleFactor ) + canvasSize / 2 - 128 * 2 ** m.zoomlevel / 2 * scaleFactor)
-    # coords = ".".join([str(a - canvasSize / 2) for a in m.centerxz] + [dimension])
+
 
     logging.info("Stitching map at %s, %s zoom level %s in %s", x, z, zoom, dimDict[dimension])
+
+    # create an image from the .dat data
     m.genimage()
+
+    # rescale the image base on the zoom level
     m.rescale(num=scaleFactor)
 
     #memory_area = io.BytesIO()
@@ -258,50 +284,68 @@ for m in mapFileObjs:
     #image_str = memory_area.getvalue()
     #im_mask_from_memory = pyvips.Image.new_from_buffer(image_str, "")
 
+    # add the image to the appropriate output map
     background[dimension].paste(m.im, (x, z), m.im)
     #bg[dimension].insert(im_mask_from_memory, x, z)
     
     # m.saveimagepng(os.path.join(webdata, "newmap", imageFile))
-    
-
-
-
     #img = Image.open('/pathto/file', 'r')
-    
     #img_w, img_h = img.size
-    
     #bg_w, bg_h = background.size
     #offset = ((bg_w - img_w) / 2, (bg_h - img_h) / 2)
 
 
 
 logging.info("Adding POI to stiched map")
+
+# iterate over the tags to add POI to map
 for tag in sorted(taggedPois):
+    # iterate over POI in tag
     for poi in taggedPois[tag]:
+        
+        # get POI player head 
         response = requests.get("https://crafatar.com/avatars/{}?size=8".format(poi[0]))
 
+        #create a new base POI
         imgTest = PIL.Image.new("RGBA", (24, 12), "black")
 
+        # object to draw on base POI
         draw = PIL.ImageDraw.Draw(imgTest)
+
+        # get the number of colors in POI
         totalColors = len(poi[7])
+
+        # calculate how high a color bar will be 
         rectHeight = 12 // totalColors
+
+        # iterate over each color
         for each in enumerate(poi[7]):
-            
+            # draw a color bar on base POI
             point1 = (0, rectHeight * each[0])
             point2 = (11, rectHeight * each[0] + rectHeight)
             draw.rectangle([point1, point2], fill=each[1])
+            
+        # add the POI id as a number
         draw.text((14, 2), str(poi[6]), font=PIL.ImageFont.truetype(fontpath,8))
+
+        # add the player head
         imgTest.paste(PIL.Image.open(io.BytesIO(response.content)), (2, 2))
+        
         #mask = imgTest.convert("L").point(lambda x: min(x, 100))
         #imgAlpha = imgTest
         #imgAlpha.putalpha(mask)
         #imgBorder = PIL.Image.new("RGBA", (26, 10), "black")
         #imgBorder.paste(imgTest, (1,1))
+
+        # add the POI to the output image
         background[0].paste(imgTest, (poi[2] * scaleFactor + canvasSize // 2 - 12, poi[3] * scaleFactor + canvasSize // 2 - 4))
+
+        # save the base POI slightly scaled up for index.md
         imgOut = imgTest.resize((48, 24))
         imgOut.save(os.path.join(papyriOutputPath, poi[0] + str(poi[6]) + ".png"), "png")
 
-        
+
+# create the dimension output folder if they don't exsist
 for d in dimDict:
     mapOutputPath = os.path.join(papyriOutputPath, "map", "dim", dimDict[d])
     if not os.path.exists(mapOutputPath):
@@ -309,23 +353,33 @@ for d in dimDict:
         os.makedirs(mapOutputPath)
 
 
-        
+# iterate over each dimension
 for d in dimDict:
+    # set the output path for map
     mapOutputPath = os.path.join(papyriOutputPath, "map", "dim", dimDict[d])
+
+    # set the output path for PNG
     outPngFile = os.path.join(mapOutputPath, 'out{}.png'.format(d))
 
     logging.info("Saving .png for %s", dimDict[d])
+
+    # save the PNG
     background[d].save(outPngFile)
 
+    # set the output path of DZI
     outputDir = os.path.join(mapOutputPath, '{}_files'.format(dimDict[d]))
 
+    # create a vips image
     dz = pyvips.Image.new_from_file(outPngFile, access='sequential')
+
+    # delete the DZI if it exsists
     if os.path.exists(outputDir):
         shutil.rmtree(outputDir)
         
     logging.info("Saving DZI for %s", dimDict[d])
-    dz.dzsave(os.path.join(mapOutputPath, '{}'.format(dimDict[d])), suffix=".png")
-    #bg[d].dzsave(os.path.join(webdata, "newmap", 'out{}'.format(d)), suffix=".png")
 
-    
+    # save the DZI
+    dz.dzsave(os.path.join(mapOutputPath, '{}'.format(dimDict[d])), suffix=".png")
+
+    #bg[d].dzsave(os.path.join(webdata, "newmap", 'out{}'.format(d)), suffix=".png")
     #bg.write_to_file("test.png")
