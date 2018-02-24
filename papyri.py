f8d1bc6dcb4e09a8878f912f4e94b780625af7c1
Jason Green <jason@green.io>
2018-02-24 15:57:28 -0500

    added POI instructions link

--- papyri.py
+++ papyri.py
@@ -694,7 +694,7 @@ for d in dimDict:
 if poi:
     # write the papyri.md file containing all the POI
     with open(os.path.join(papyriOutputPath, "papyri.md"), "w", encoding="utf-8") as poisFile:
-
+        poisFile.write("[POI instructions](https://github.com/jason-green-io/papyri/blob/master/README.md#poi-instructions)")
             #poisFile.write("### Map stats\n")
             #poisFile.write(mapStatsStr)
             logging.info("Writing POI to papyri.md")
