55414ddda3dd7c84c7c3517f5dcc9f8a689953ed
Jason Green <jason@green.io>
2018-03-12 21:40:20 -0400

    oops

--- papyri.py
+++ papyri.py
@@ -514,7 +514,13 @@ for d in dimDict:
 
             logging.info("Converting png to deepzoom")
             pyvips.Image.new_from_file(outPngFile, access='sequential').dzsave(os.path.join(mapOutputPath, "temp"), suffix=".png")
+            if os.path.exists(os.path.join(mapOutputPath, bigMapName + ".dzi")):
+                shutil.rmtree(os.path.join(mapOutputPath, bigMapName + ".dzi"))
+                
             shutil.move(os.path.join(mapOutputPath, "temp.dzi"), os.path.join(mapOutputPath, bigMapName + ".dzi"))
+            if os.path.exists(os.path.join(mapOutputPath, bigMapName + "_files")):
+                shutil.rmtree(os.path.join(mapOutputPath, bigMapName + "_files"))
+                
             shutil.move(os.path.join(mapOutputPath, "temp_files"), os.path.join(mapOutputPath, bigMapName + "_files"))
                                      
 
